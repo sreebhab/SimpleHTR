7f49e9c1ec41439ed5d84080ab20de77fa8b9dd0
sreebhab <sreebha@gmail.com>
2019-05-11 09:06:21 +0530

    Replaced model path to work in colab

--- src/Model.py
+++ src/Model.py
@@ -120,8 +120,8 @@ class Model:
 
 			# prepare information about language (dictionary, characters in dataset, characters forming words) 
 			chars = str().join(self.charList)
-			wordChars = open('../model/wordCharList.txt').read().splitlines()[0]
-			corpus = open('../data/corpus.txt').read()
+			wordChars = open('/content/SimpleHTR/model/wordCharList.txt').read().splitlines()[0]
+			corpus = open('/content/SimpleHTR/data/corpus.txt').read()
 
 			# decode using the "Words" mode of word beam search
 			self.decoder = word_beam_search_module.word_beam_search(tf.nn.softmax(self.ctcIn3dTBC, dim=2), 50, 'Words', 0.0, corpus.encode('utf8'), chars.encode('utf8'), wordChars.encode('utf8'))
@@ -135,7 +135,7 @@ class Model:
 		sess=tf.Session() # TF session
 
 		saver = tf.train.Saver(max_to_keep=1) # saver saves model to file
-		modelDir = '../model/'
+		modelDir = '/content/SimpleHTR/model/'
 		latestSnapshot = tf.train.latest_checkpoint(modelDir) # is there a saved model?
 
 		# if model must be restored (for inference), there must be a snapshot
@@ -243,5 +243,5 @@ class Model:
 	def save(self):
 		"save model to file"
 		self.snapID += 1
-		self.saver.save(self.sess, '../model/snapshot', global_step=self.snapID)
+		self.saver.save(self.sess, '/content/SimpleHTR/model/snapshot', global_step=self.snapID)
  
